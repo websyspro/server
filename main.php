d748b034aa5cd36c640a5c5450d0cb5fc32d16ad
e.thiago-pla <e.thiago-pla@platos.parceirosedu.com.br>
2026-04-17 18:54:25 -0300

    Atualizar componentes

--- main.php
+++ main.php
@@ -1,10 +1,6 @@
 <?php
 
 use Websyspro\Server\Application;
-use Websyspro\Server\Applications\ShopsModule;
-use Websyspro\Server\Applications\AccountsModule;
 
 Application::Modules([
-  ShopsModule::class,
-  AccountsModule::class
 ]);
