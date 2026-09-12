15b00b7dc40edceaa6596467aaa0b97a9ed074ab
Emerson Thiago Sousa Albuquerque <e.thiago-pla@platos.parceirosedu.com.br>
2026-09-12 17:31:34 -0300

    Release 1.0.4

--- src/index.php
+++ src/index.php
@@ -1,3 +1,13 @@
 <?php
 
+/**
+ *  Define DevTools_Base_Dir
+ */
+defined( "DevTools_Base_Dir" ) || define(
+  "DevTools_Base_Dir", realpath(
+    dirname( __DIR__ ) 
+  ) . DIRECTORY_SEPARATOR
+);
+
+echo DevTools_Base_Dir . PHP_EOL;
 echo "My Apps";
