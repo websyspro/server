764b8b3df1b3288733eed53709c096eed4640c2d
Emerson Thiago Sousa Albuquerque <e.thiago-pla@platos.parceirosedu.com.br>
2026-09-14 11:06:56 -0300

    Release 1.0.18

--- src/index.php
+++ src/index.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * WebSocket Server Runtime
+ * 
+ * Runtime interno para iniciar o servidor WebSocket.
+ * Não deve ser exportado como binário do Composer.
+ * Usado pelo BrowserReloadHandler para rodar em processo separado.
+ */
+defined( "DevTools_Base_Dir" ) || define(
+  "DevTools_Base_Dir", realpath(
+    dirname( __DIR__, 1 ) 
+  ) . DIRECTORY_SEPARATOR
+);
+
 /*
  * Autoload
  * Main
