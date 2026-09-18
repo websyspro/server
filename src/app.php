7a5be0b751e0082b65ad658f1ce286db012e83a7
Emerson Thiago Sousa Albuquerque <e.thiago-pla@platos.parceirosedu.com.br>
2026-09-18 15:18:54 -0300

    Release 1.0.29

--- src/app.php
+++ src/app.php
@@ -1,8 +1,7 @@
 <?php
 
-use Websyspro\Server\Request;
 use Websyspro\Server\WorkerServer;
 
 $ws = new WorkerServer(); 
-$ws->get( "/test/:id", fn( Request $request ) => $request->body );
+$ws->get( "/health", fn() => "Server running" );
 $ws->start();
