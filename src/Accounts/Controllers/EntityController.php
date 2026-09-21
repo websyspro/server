516eb3f4e7dc1e58624ea882d413c4c6e894cb31
Websyspro <cpd.emersontsa@gmail.com>
2026-09-21 19:03:07 -0300

    Release 1.0.65

--- src/Accounts/Controllers/EntityController.php
+++ src/Accounts/Controllers/EntityController.php
@@ -3,6 +3,12 @@
 namespace Websyspro\Server\Accounts\Controllers;
 
 use ReflectionClass;
+use Websyspro\Entity\Schemas\MySqlEntityStructure;
+use Websyspro\Entity\Schemas\MySqlEntityStructurePersisteds;
+use Websyspro\Entity\Schemas\MySqlSchemaManager;
+use Websyspro\Entity\Schemas\PostgresEntityStructure;
+use Websyspro\Entity\Schemas\PostgresEntityStructurePersisteds;
+use Websyspro\Entity\Schemas\PostgresSchemaManager;
 use Websyspro\Entity\Schemas\SqlServerEntityStructure;
 use Websyspro\Entity\Schemas\SqlServerEntityStructurePersisteds;
 use Websyspro\Entity\Schemas\SqlServerSchemaManager;
@@ -10,6 +16,7 @@ use Websyspro\Server\Accounts\Entities\TestEntity;
 use Websyspro\WorkerServer\Decorators\Body;
 use Websyspro\WorkerServer\Decorators\Controller;
 use Websyspro\WorkerServer\Decorators\Get;
+use Websyspro\WorkerServer\Decorators\Param;
 
 
 
@@ -29,34 +36,48 @@ class EntityController
     return $structureEntity;
   }
   
-  #[Get("/async")]
+  #[Get("/async/:fonte")]
   public function async(
+    #[Param("fonte")] string $fonte
   ): mixed {
-    // $schemaManager = new MySqlSchemaManager(
-    //   new MySqlEntityStructure(
-    //     new ReflectionClass(
-    //       TestEntity::class
-    //     )
-    //   ),
-    //   new MySqlEntityStructurePersisteds(
-    //     new ReflectionClass(
-    //       TestEntity::class
-    //     )
-    //   ) 
-    // );
-
-    $schemaManager = new SqlServerSchemaManager(
-      new SqlServerEntityStructure(
-        new ReflectionClass(
-          TestEntity::class
-        )
+     $schemaManager = match( $fonte ){
+      "mysql" => new MySqlSchemaManager(
+        new MySqlEntityStructure(
+          new ReflectionClass(
+            TestEntity::class
+          )
+        ),
+        new MySqlEntityStructurePersisteds(
+          new ReflectionClass(
+            TestEntity::class
+          )
+        ) 
       ),
-      new SqlServerEntityStructurePersisteds(
-        new ReflectionClass(
-          TestEntity::class
-        )
-      ) 
-    );    
+      "postgres" => new PostgresSchemaManager(
+        new PostgresEntityStructure(
+          new ReflectionClass(
+            TestEntity::class
+          )
+        ),
+        new PostgresEntityStructurePersisteds(
+          new ReflectionClass(
+            TestEntity::class
+          )
+        ) 
+      ),
+      "sqlserver" => new SqlServerSchemaManager(
+        new SqlServerEntityStructure(
+          new ReflectionClass(
+            TestEntity::class
+          )
+        ),
+        new SqlServerEntityStructurePersisteds(
+          new ReflectionClass(
+            TestEntity::class
+          )
+        ) 
+      )
+    };   
 
     $schemaManager->asyncEntity();
     return $schemaManager;
